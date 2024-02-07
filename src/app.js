1dab72483ee0c7bc7fab63afd820b6ef5a543f11
Vikram9392 <vikramvallala12@gmail.com>
2024-02-07 11:30:18 +0530

    done

--- src/app.js
+++ src/app.js
@@ -10,31 +10,47 @@ app.use(bodyParser.json());
 // Mock user data (replace with a database in a real application)
 const users = [
   // Define user objects here
+  { id: 1, username: 'user1', password: 'password1' },
+
+  { id: 2, username: 'user2', password: 'password2' },
 ];
 
 // Authentication endpoint (students should implement this)
 app.post('/login', (req, res) => {
   // Implement user authentication logic here
+  const{username,password}=req.body;
+  const user=user.find((user)=>user.username==username&&user.password==password)
   if (!user) {
     return res.status(401).json({ message: 'Authentication failed' });
   }
 
   // If authentication is successful, generate a JWT token and send it in the response
   // Example token generation:
-  // const token = jwt.sign({ userId: user.id, username: user.username }, secretKey);
-  // res.status(201).json({ token });
+  const token = jwt.sign({ userId: user.id, username: user.username }, secretKey);
+  res.status(201).json({ token });
 });
 
-// Protected route (students should implement this)
-app.get('/profile', (req, res) => {
+function isAuthorized(req,res,next){
+  const token=req.headers.authorization.split(' ')[1];
   if (!token) {
     return res.status(401).json({ message: 'Authorization required' });
   }
+  jwt.verify(token,secretKey,(err,decoded)=>{
+    if(err){
+      return res.status(401).json({message:'Invalid Token',error:err.message})
+    }
+  })
+  req.user=decoded;
+  next();
+}
+// Protected route (students should implement this)
+app.get('/profile',isAuthorized,(req, res) => {
+  
   // Middleware to check for a valid JWT token
   // Implement JWT token verification logic here
   // If the token is valid, students can access the user's data from 'decoded'
   // Example response:
-  // res.status(201).json({ message: 'Profile data', user: decoded });
+  res.status(201).json({ message: 'Profile data', user: req.user });
 });
 
 module.exports = app;
